76e787d66c16a7f0273d4ea9c3199384d568a23b
cmal <paul@cmal.info>
2013-02-28 00:43:29 +0000

    Added points management. Added a few colors. Changed speed of IA player.

--- test/source/movement.h
+++ test/source/movement.h
@@ -8,76 +8,81 @@
 
 
 float random(float low, float high){
-  // Return a random float
-  return((float)rand()/(float)RAND_MAX/(high-low));
+    // Return a random float
+    return((float)rand()/(float)RAND_MAX/(high-low));
 }
 
 bool isUp(Circle c){
-  // Returns whether the circle's middle is inside the screen
-  // (from the bottom)
-  return(c.y - c.radius < 0);
+    // Returns whether the circle's middle is inside the screen
+    // (from the bottom)
+    return(c.y - c.radius < 0);
 }
 
 bool isDown(Circle c){
-  // Returns whether the circle's middle is inside the screen
-  // (from the top)
-  return(c.y + c.radius >= maxY);
+    // Returns whether the circle's middle is inside the screen
+    // (from the top)
+    return(c.y + c.radius >= maxY);
 }
 
 double position(Circle c, Button b){
-  return((b.y + b.height/2) - c.y);
+    return((b.y + b.height/2) - c.y);
 }
 
 void isMoving(Circle& move, Button r, Button l, int& pass,
-	      float& speed, float& b){
-  if(isUp(move)){
-    b = -b;
-    move.y = move.radius;
-  }
-  if(isDown(move)){
-    b = -b;
-    move.y = 480 - move.radius;
-  }
-  if(isIn(move,r)){
-    float i1 = -position(move,r);
-    i1 = i1 / (r.height/2);
-    b = (i1*speed);
-    pass = 1;
-    move.x = r.x - move.radius;
-    speed += 0.5;
-  }
-  if(isIn(move,l)){
-    float i1 = position(move,l);
-    i1 = i1 / (l.height/2);
-    b = (-i1*speed);
-    pass = 0;
-    move.x = move.radius + l.width;
-    speed += 0.5;
-  }
+        float& speed, float& b){
+    if(isUp(move)){
+        // Ball has reached top, needs to go back down
+        b = -b;
+        move.y = move.radius;
+    }
+    if(isDown(move)){
+        // Ball has reached bottom, needs to go back up
+        b = -b;
+        move.y = 480 - move.radius;
+    }
+    if(isIn(move,r)){
+        // Ball has hit the right paddle
+        float i1 = -position(move,r);
+        i1 = i1 / (r.height/2);
+        b = (i1*speed);
+        pass = 1;
+        move.x = r.x - move.radius;
+        speed += 0.5;
+    }
+    if(isIn(move,l)){
+        // Ball has hit the left paddle
+        float i1 = position(move,l);
+        i1 = i1 / (l.height/2);
+        b = (-i1*speed);
+        pass = 0;
+        move.x = move.radius + l.width;
+        speed += 0.5;
+    }
+    // Move the ball
     if(pass == 0){
-      move.x = move.x + speed;
-      move.y = move.y + b;
+        move.x = move.x + speed;
+        move.y = move.y + b;
     }
     if(pass == 1){
-      move.x = move.x - speed;
-      move.y = move.y + b;
-  }
+        move.x = move.x - speed;
+        move.y = move.y + b;
+    }
 }
 
 
 void ia(Button& b, Circle c, float v){
-  float rand = 0.0;
-  if(b.y + b.height > maxY)
-    b.y = maxY- b.height;
-  if(b.y < 0)
-    b.y = 0;
-  if(c.x - b.x <= maxX/2+maxX/4){
-    float pos = c.y - b.y + rand;
-    if(pos > 0.0)
-      b.y += v ;
-    if(pos < 0.0)
-      b.y -= v ;
-  }
+    float rand = 0.0;
+    if(b.y + b.height > maxY)
+        b.y = maxY- b.height;
+    if(b.y < 0)
+        b.y = 0;
+    if(c.x - b.x <= maxX/2+maxX/4){
+        float pos = c.y - b.y + rand;
+        if(pos > 0.0)
+            b.y += 0.75 * v;
+        if(pos < 0.0)
+            b.y -= 0.75 * v;
+    }
 }
 
 #endif
