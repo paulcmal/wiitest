0b2167e50c3949831bb02106fc488c1b8a2e1d9b
RaVhen <raven-19@gmx.fr>
2013-03-28 17:23:16 +0100

    Saut de Snake + Sprite de test

--- MGS/build/codec_png.h
+++
@@ -1,3 +0,0 @@
-extern const u8 codec_png_end[];
-extern const u8 codec_png[];
-extern const u32 codec_png_size;
--- MGS/build/menu3_png.h
+++
@@ -1,3 +0,0 @@
-extern const u8 menu3_png_end[];
-extern const u8 menu3_png[];
-extern const u32 menu3_png_size;
